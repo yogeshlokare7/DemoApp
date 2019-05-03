8a39ef2e072b44a9f6f9ff9b28d9c83139a7be29
Yogesh Lokare <yogeshlokare7@gmail.com>
2019-05-03 19:31:20 +0530

    Added Utility Package

--- demoapp/src/main/java/com/tbsm/model/User.java
+++ demoapp/src/main/java/com/tbsm/model/User.java
@@ -2,19 +2,12 @@ package com.tbsm.model;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -117,9 +110,10 @@ public class User implements Serializable{
     
     @Column(name = "societyid")
     private Long societyid;
-	
 
-    public User() {}
+    public User() {
+    	
+    }
 
     public User(String firstname, String lastname, String username, String email, String password, Character status) {
         this.firstname = firstname;
---
+++ demoapp/src/main/java/com/tbsm/utils/ApplicationUtility.java
@@ -0,0 +1,12 @@
+package com.tbsm.utils;
+
+public class ApplicationUtility {
+
+	private ApplicationUtility() {
+
+	}
+	public static final String LOGIN_URL="http://localhost:4200/#/admin/login";
+	public static final String FORGOT_URL = "http://localhost:4200/#/admin/setpassword;resetToken=";
+	public static final String RESET_TOKEN = "http://localhost:4200/#/admin/forgot?resetToken=";
+	public static final String APP_URL = "http://localhost:4200/#/admin/forgot?resetToken=";
+}
